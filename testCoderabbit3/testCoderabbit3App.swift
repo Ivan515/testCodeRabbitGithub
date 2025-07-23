1364f677530b625b81f025819ad7915342993b13
Ivan <van.ka515@gmail.com>
2025-07-23 17:06:29 +0300

    Initial commit

---
+++ testCoderabbit3/testCoderabbit3App.swift
@@ -0,0 +1,17 @@
+//
+//  testCoderabbit3App.swift
+//  testCoderabbit3
+//
+//  Created by Ivan Apet on 23.07.25.
+//
+
+import SwiftUI
+
+@main
+struct testCoderabbit3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
