4b8fbddab7c5092d3369c68ff99c006510f3d1ef
Ivan <van.ka515@gmail.com>
2025-07-23 17:16:46 +0300

    Add feature 1

--- testCoderabbit3/ContentView.swift
+++ testCoderabbit3/ContentView.swift
@@ -7,18 +7,50 @@
 
 import SwiftUI
 
+//
+//  ContentView.swift
+//  TestCodeRabbit
+//
+//  Created by Ivan Apet on 23.07.25.
+//
+
+import SwiftUI
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [Color.blue, Color.purple]), startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Text("Welcome to SwiftUI")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .padding()
+                
+                Text("Create beautiful user interfaces with ease.")
+                    .font(.subheadline)
+                    .foregroundColor(.white)
+                    .padding(.bottom, 50)
+                
+                Button(action: {
+                    print("Button tapped!")
+                }) {
+                    Text("Get Started")
+                        .fontWeight(.semibold)
+                        .padding()
+                        .background(Color.white)
+                        .foregroundColor(.blue)
+                        .cornerRadius(10)
+                }
+            }
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
